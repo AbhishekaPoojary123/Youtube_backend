32c52e4ab6702f37157238697d43fa99b1593ae6
abhisheka <abhishekapoojary123@gmail.com>
2026-05-17 18:26:13 +0530

    added jwt access token and refresh token and bcrypt for lpassword hasing

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import connetcDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connetcDB();
+connetcDB()
+    .then(() => {
+        application.listen(process.env.PORT || 8000, () => {
+            console.log(`app is listening on port ${process.env.PORT || 8000}`);
+        });
+    })
+    .catch((error) => {
+        console.log("mongoDB connection failed", error);
+    });
