ce7647473de0f21c6f2f88ea813596ed7ca35e07
abhisheka <abhishekagit@gmail.com>
2026-06-07 17:50:19 +0530

    fix: made backedn and frintend folder structure as like production leve

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Register from "./views/auth/pages/Register";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
