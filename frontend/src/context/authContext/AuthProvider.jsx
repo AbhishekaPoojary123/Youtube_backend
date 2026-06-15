a5e0bff0ec4456cc9506c10c0b4ba8a81984ff64
abhisheka <abhishekagit@gmail.com>
2026-06-15 22:07:54 +0530

    feat: added current-user api in authContext

--- frontend/src/context/authContext/AuthProvider.jsx
+++ frontend/src/context/authContext/AuthProvider.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import { api } from "../../services/api";
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCurrentUser = async () => {
+            try {
+                const response = await api.post("/users/current-user");
+                setUser(response.data.data);
+            } catch (error) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchCurrentUser();
+    }, []);
 
     return (
-        <AuthContext.Provider value={{ user, setUser }}>
+        <AuthContext.Provider value={{ user, setUser, loading }}>
             {children}
         </AuthContext.Provider>
     );
